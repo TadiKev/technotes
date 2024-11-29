3d3950702942c86cf8def98ad0c0daccf919e4f0
Kelvin <machakakelvin903@gmail.com>
2024-11-29 21:53:14 +0200

    Updated backend URL for deployed environment

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../../features/auth/authSlice'; 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-api.conrender.com',
+    baseUrl: 'https://technotes-api-1-2opk.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
